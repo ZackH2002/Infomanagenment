c27c3cbef5b2c09ed1f6004fb995ed47a21ecbb4
hyckxx <1415486250@qq.com>
2022-06-06 17:13:11 +0800

    feature:完善图片上传

--- src/main/java/com/zack/service/PicUpload.java
+++ src/main/java/com/zack/service/PicUpload.java
@@ -27,27 +27,14 @@ public class PicUpload {
         ResultVO result = new ResultVO();
         // 获取原文件名字
         String originName = file.getName();
-        // 生成新文件名字 用uuid防止重复
-        String uuid = IdUtil.simpleUUID();
         // 获取文件后缀名
         String suffix = originName.substring(originName.lastIndexOf(".") + 1);
-
+        // 判断文件是否合法
         if (!"jpg".equals(suffix) && !"png".equals(suffix)) {
             return result.message("仅支持.jpg和.png类型的图片!").code(400);
         }
-        // 最终的文件名字
-        String fileName = uuid + "." + suffix;
-        // 文件最终的储存路径
-        String filePath = UPLOAD_PATH + fileName;
-        File newFile = new File(filePath);
-        // 判断文件是否已经存在
-        if (!newFile.getParentFile().exists()) {
-            // 不存在则创建文件
-            if (!newFile.mkdirs()) {
-                return result.message("操作失败!").code(500);
-            }
-        }
-        map.put("url",filePath);
+        // 将文件url存入数据库
+
         return result.message("操作成功!").code(200).data(map);
     }
 
